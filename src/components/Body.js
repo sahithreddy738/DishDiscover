ec3e169ac5af22b0dcb5d237ee8c289bba107552
Sahith Reddy <sahithreddy738@gmail.com>
2024-07-14 16:31:50 +0530

    assignment-5 added search filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,24 @@
 import { RESTAURANT_LIST } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body =() =>{
+    const [listOfRestaurants,setListOfRestaurants]=useState(RESTAURANT_LIST);
+    const [input,setInput]=useState("");
+    const handleClick=() => {
+        const filteredData=listOfRestaurants.filter(restaurant=>restaurant.info.avgRating>4);
+        setListOfRestaurants(filteredData);
+    }
+    const handleChange=(e) => {
+       setInput(e.target.value);
+    }
     return (
         <div className="body-container">
-             <input placeholder="search here"></input>
+             <input placeholder="search restaurants" className="input-field" onChange={handleChange}></input>
+             <button className="rated-btn" children="Top-Rated"  onClick={handleClick}></button>
              <div className="rest-container">
                  {
-                    RESTAURANT_LIST.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
+                    listOfRestaurants.filter(restaurant=>restaurant.info.name.toLowerCase().includes(input)).map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
                  }
              </div>
 
